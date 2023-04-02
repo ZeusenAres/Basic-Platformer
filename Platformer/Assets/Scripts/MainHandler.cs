2239ebc3c465e4b360682ccc6c2a4d4e1e69836c
ZeusenAres <2060537@talnet.nl>
2023-04-03 00:41:41 +0200

    made faulty changes

--- Platformer/Assets/Scripts/MainHandler.cs
+++ Platformer/Assets/Scripts/MainHandler.cs
@@ -6,14 +6,9 @@ using UnityEngine;
 public class MainHandler : MonoBehaviour
 {
 
-    private Player player;
-    private string jsonFile = "Assets/Scripts/JSON/Items.json";
-
     void Start()
     {
 
-        string json = File.ReadAllText(jsonFile);
-        Debug.Log(player = JsonUtility.FromJson<Player>(json));
         Time.timeScale = 1.0f;
     }
 }
