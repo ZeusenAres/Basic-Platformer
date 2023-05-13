473ccc8744c633d8bb0b8d2361d9de5e4c52e00c
ZeusenAres <2060537@talnet.nl>
2023-05-14 00:26:22 +0200

    changed player package handler

---
+++ Platformer/Assets/Scripts/ContentHandler.cs
@@ -0,0 +1,34 @@
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Globalization;
+using System.Reflection;
+using System.Text.RegularExpressions;
+using UnityEngine;
+
+public class ContentHandler : MonoBehaviour
+{
+    private string matchedProperty;
+    void Start()
+    {
+        var targetClass = GameObject.Find("Main Camera").GetComponent("Camera");
+        //Debug.Log(type);
+        //var type = typeof(camera);
+        Type type = targetClass.GetType();
+        foreach (var property in type.GetProperties())
+        {
+
+            if(property.Name.StartsWith("backgroundColor"))
+            {
+
+                matchedProperty = property.Name;
+                break;
+            }
+        }
+        PropertyInfo propertyInfo = type.GetProperty(matchedProperty);
+        propertyInfo.SetValue(targetClass, Color.gray);
+    }
+}
